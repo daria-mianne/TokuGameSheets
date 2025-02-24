16cdac434a573fe5ac2529efd5bc5eaf1e264852
Daria Anderson <dariaander@icloud.com>
2025-02-23 18:11:05 -0800

    Add explicit http and https server listeners

--- server.ts
+++ server.ts
@@ -1,5 +1,12 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+
+const privateKey = fs.readFileSync('ssl/server.key', 'utf8');
+const certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
 const __dirname = import.meta.dirname;
@@ -17,7 +24,12 @@ app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// Listen for HTTP traffic
+const httpPort = process.env.HTTP_PORT || 5000;
+const httpServer = http.createServer(app);
+httpServer.listen(httpPort);
+
+// Listen for HTTPS traffic
+const httpsPort = process.env.HTTPS_PORT || 5080;
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(httpsPort);
