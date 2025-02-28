f362a2d5dd371fe7a49f73c90b90e793e9027299
Daria Anderson <dariaander@icloud.com>
2025-02-27 18:30:36 -0800

    breaks the containerized app locally, but fixes for remote. another 'good enough for now' commit

--- client/src/loadmain.ts
+++ client/src/loadmain.ts
@@ -1,4 +1,4 @@
 const script = document.createElement('script');
-const scriptPath = '/src/main.tsx';
+const scriptPath = '/src/main.tsx'; // TODO: load from localhost if dev mode is enabled
 script.setAttribute('src', scriptPath);
 document.head.appendChild(script);
