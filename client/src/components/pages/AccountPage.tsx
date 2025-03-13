55c950afe85fd4169310f41a5f691af0130e9aa9
Daria Anderson <dariaander@icloud.com>
2025-03-12 17:29:33 -0700

    Fix account page logged-in check

--- client/src/components/pages/AccountPage.tsx
+++ client/src/components/pages/AccountPage.tsx
@@ -1,7 +1,15 @@
-export default function AccountPage() {
-    const user = window.currentUser;
+import { useMemoryOnlyDataStore } from "@datastore/memoryOnlyData";
+import { useEffect, useState } from "preact/hooks";
 
-    if (!user) {
+export default function AccountPage() {
+    const [showNotLoggedInWarning, setShowNotLoggedInWarning] = useState(false);
+    const { currentUser } = useMemoryOnlyDataStore();
+
+    useEffect(() => {
+        setShowNotLoggedInWarning(!currentUser);
+    }, [currentUser]);
+
+    if (showNotLoggedInWarning) {
         return <p>Not logged in</p>;
     }
 
@@ -10,15 +18,15 @@ export default function AccountPage() {
             <h1>My Account</h1>
             <p>Currently, there is no way to edit your account. This will come in a future update.</p>
             <h2>Username</h2>
-            {user.username}
+            {currentUser?.username}
             <h2>Display Name</h2>
-            {user.displayName}
+            {currentUser?.displayName}
             <h2>Email</h2>
-            {user.recoveryEmail}
+            {currentUser?.recoveryEmail}
             <h2>Is Admin?</h2>
-            {String(user.isAdmin) /* need explicit string conversion for boolean here */}
+            {String(currentUser?.isAdmin) /* need explicit string conversion for boolean here */}
             <h2>Account Creation Date</h2>
-            {user.createdAt}
+            {currentUser?.createdAt}
         </>
     );
 }
